d1a1d1a3567a5e8aa44a17b96a6c7ea5d560eae4
NikolaevKirill <nikolaev.kirill.p@gmail.com>
2022-04-04 12:15:58 +0300

    New update

--- qlearn.py
+++ qlearn.py
@@ -1,33 +1,92 @@
 import numpy as np
+#  from tqdm import tqdm
 
 
 class Qagent:
-    def __init__(self):
+    def __init__(self, bounds, num_env_discret_values, num_act_discret_values):
         """
         Инициализация Q-агента. Задание параметров обучения.
+        bounds: np.array([x, v, theta, w]), текущее состояние среды
+        num_env_discret_values: list or int, if list - количество значений каждого параметра среды,
+                                            if int - одинаковое количество значений каждого параметра среды
+        num_act_discret_values: int, количество действий
         """
 
         self.gamma = 0.95
         self.alpha = 0.8
         self.epsilon = 0.2
         self.q_table = {}
-        self.states = []
-        self.actions = []
+        self.lengths_games = []
+        self.num_actions = num_act_discret_values
 
-    def create_q_table(self, states, actions):
-        """
-        Функция создаёт q-table, с нулевыми значениями q.
-        Реализация: dict{(state):[[q-value, action1], ..., [q-value, action_last]}.
-        states: list(tuple(state), ..., tuple(state),), список с кортежами параметров среды
-        actions: list, список с действиями
-        """
+        if not type(bounds) is np.ndarray:
+            bounds = np.array(bounds)
 
-        q_and_actions = [list(par) for par in zip(np.zeros(len(actions)), actions)]
-        q_table = {state: q_and_actions for state in states}
+        if len(bounds.shape) == 1:
+            bounds = np.array([-bounds, bounds])
 
-        self.states = states
-        self.actions = actions
-        self.q_table = q_table
+        self.num_params_of_state = bounds.shape[1]
+
+        if type(num_env_discret_values) is int:
+            num_env_discret_values = [num_env_discret_values] * self.num_params_of_state
+
+        self.discret_values = []
+
+        for i in range(self.num_params_of_state):
+            bins = np.linspace(bounds[0][i], bounds[1][i], num_env_discret_values[i])
+            self.discret_values.append(bins)
+
+        self.actions = np.array(range(num_act_discret_values))
+
+    def fit(self, env, epochs, alpha=0.1, epsilon=1, gamma=0.85, inv_t=10, max_length_game=1000):
+
+        '''
+        Функция обучения агента
+        env: среда, с которой взаимодействует агент
+        epochs: int, количество игр
+        alpha: float, скорость обучения
+        epsilon: float, начальное значения epsilon
+        gamma: float, коэффициент дисконтирования
+        inv_t: float, коэффициент затухания epsilon
+        max_length_game: int, максимальное количество игр
+        '''
+
+        self.lengths_games = []
+
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.gamma = gamma
+
+        for i_episode in range(epochs):  # tqdm(range(epochs)):
+
+            t = 0
+
+            observation = env.reset()
+            self.epsilon *= np.exp(-i_episode / epochs * inv_t)
+
+            while True:
+
+                # if i_episode+1 == epochs:
+                #    env.render()
+
+                action = self.strategy(observation)
+                new_observation, reward, done, _ = env.step(action)
+                t += 1
+
+                if done or (t > max_length_game):
+                    self.lengths_games.append(t)
+                    break
+
+                self.update_q_value(observation, action, new_observation, reward + observation[2] * 2)
+                observation = new_observation
+
+            print(
+                f'''Номер игры:{i_episode}, Медиана длительности игр:{np.median(self.lengths_games)}, 
+                Мин.длит.игр:{np.min(self.lengths_games)}''',
+                end='\r')
+
+        # self.lengths_games = length_of_game
+        # env.close()
 
     def strategy(self, state):
         """
@@ -37,12 +96,15 @@ class Qagent:
         """
 
         q_table = self.q_table
-        q_and_action = q_table[state]
-        q_values = [q for q, action in q_and_action]
-        actions = list(range(len(q_values)))
+        observation = tuple(self.get_observation(state))
+        q_values = q_table.setdefault(observation)
 
-        if (np.random.rand() < self.epsilon) or (np.sum(q_values) == 0):
-            action = np.random.choice(actions)
+        if q_values is None:
+            q_values = np.zeros(self.num_actions)
+            q_table[observation] = q_values
+
+        if np.random.rand() < self.epsilon:
+            action = np.random.choice(self.actions)
         else:
             action = np.argmax(q_values)
 
@@ -59,13 +121,35 @@ class Qagent:
         """
 
         q_table = self.q_table
-        q_values_of_new_state = [q for q, action in q_table[new_state]]
+        observation = tuple(self.get_observation(state))
+        new_observation = tuple(self.get_observation(new_state))
+
+        q_values_of_new_state = q_table.setdefault(new_observation)
+
+        if q_values_of_new_state is None:
+            q_values_of_new_state = np.zeros(self.num_actions)
+            q_table[new_observation] = q_values_of_new_state
+
         max_q_next = np.max(q_values_of_new_state)
-        q_old = q_table[state][action][0]
-        q_table[state][action][0] = q_old + self.alpha * (
-            reward + self.gamma * max_q_next - q_old
+
+        q_old = q_table[observation][action]
+        q_table[observation][action] = q_old + self.alpha * (
+                reward + self.gamma * max_q_next - q_old
         )
         self.q_table = q_table
+
+    def get_observation(self, state):
+
+        indices = []
+        bins = self.discret_values
+
+        for i in range(self.num_params_of_state):
+            ind = np.digitize(state[i], bins[i])
+            if ind == len(bins[i]):
+                ind -= 1
+            indices.append(ind)
+
+        return indices
 
     def load_q_table(self, q_table):
         """
